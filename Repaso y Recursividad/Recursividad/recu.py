0f367d76b0bcde5ff143b3510ec84fb51d59d27d
Nicolas <nicolasmacri09@gmail.com>
2024-09-17 20:36:56 -0300

    Update recu.py

--- Repaso y Recursividad/Recursividad/recu.py
+++ Repaso y Recursividad/Recursividad/recu.py
@@ -1,8 +1,10 @@
 def calcular_potencia (base: int = 2 , exponente : int = 2):
+    # calcular la potencia, en caso de no recibir nada realizar 2 al cuadrado.
     if exponente == 0:
         return 1    
     else:
         return (base * exponente) 
+# (ejemplos de posibles resultados)
 
 resultado = calcular_potencia()
 resultado2 = calcular_potencia(3)
